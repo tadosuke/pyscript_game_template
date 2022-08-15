3b494cdafb2e5929d60cc6c2ab52b356af570141
tadosuke <tadosuke@gmail.com>
2022-08-16 08:14:38 +0900

    viewのテストクラスを仮作成

--- src/view.py
+++ src/view.py
@@ -1,2 +1,35 @@
 """ゲームビュー."""
 
+from values import *
+
+
+class AbstractRenderer:
+    """描画の抽象クラス."""
+
+    @property
+    def size(self) -> Size:
+        pass
+
+    def clear(self):
+        """画面をクリアする."""
+        pass
+
+    def draw_rect(self, rect: Rect, fill_style: str) -> None:
+        """矩形の描画."""
+        pass
+
+    def draw_line(self, start_pos: tuple[int, int], end_pos: tuple[int, int], stroke_style: str) -> None:
+        """線の描画."""
+        pass
+
+    def draw_circle(self, center: tuple[int, int], radius: int, fill_style: str) -> None:
+        """円の描画."""
+        pass
+
+    def draw_image(self, image, position: Position, size: Size) -> None:
+        """画像の描画."""
+        pass
+
+    def draw_text(self, text: str, position: tuple[int, int], font: str, fill_style: str) -> None:
+        """テキストの描画."""
+        pass
---
+++ tests/test_view.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestView(unittest.TestCase):
+
+    def test_case(self):
+
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
