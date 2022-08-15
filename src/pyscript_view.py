5c60f6172250ce50d71419d0853ffc2cb61e028a
tadosuke <tadosuke@gmail.com>
2022-08-16 08:47:36 +0900

    AbstractViewを別モジュールに分離

--- src/pyscript_view.py
+++ src/pyscript_view.py
@@ -12,38 +12,7 @@ from js import (
 import typing as tp
 from model import GameModel
 from values import *
-
-
-class AbstractRenderer:
-    """描画の抽象クラス."""
-
-    @property
-    def size(self) -> Size:
-        pass
-
-    def clear(self):
-        """画面をクリアする."""
-        pass
-
-    def draw_rect(self, rect: Rect, fill_style: str) -> None:
-        """矩形の描画."""
-        pass
-
-    def draw_line(self, start_pos: tuple[int, int], end_pos: tuple[int, int], stroke_style: str) -> None:
-        """線の描画."""
-        pass
-
-    def draw_circle(self, center: tuple[int, int], radius: int, fill_style: str) -> None:
-        """円の描画."""
-        pass
-
-    def draw_image(self, image: Image, position: Position, size: Size) -> None:
-        """画像の描画."""
-        pass
-
-    def draw_text(self, text: str, position: tuple[int, int], font: str, fill_style: str) -> None:
-        """テキストの描画."""
-        pass
+from view import AbstractRenderer
 
 
 class PyScriptRenderer(AbstractRenderer):
