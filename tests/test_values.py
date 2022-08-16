613cbd6b7175660eb5bd28d6f89e42be23ef967c
tadosuke <tadosuke@gmail.com>
2022-08-16 09:04:01 +0900

    Colorクラスを追加

--- tests/test_values.py
+++ tests/test_values.py
@@ -14,6 +14,10 @@ class TestValues(unittest.TestCase):
         self.assertEqual(color.b, 30)
         self.assertEqual(color.a, 40)
 
+        with self.assertRaises(ValueError):
+            color = Color(-1, -1, -1, -1)
+            color = Color(256, 256, 256, 256)
+
 
 if __name__ == '__main__':
     unittest.main()
