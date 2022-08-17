975d235df34bb4f3063012b6fc63067d6e09ff6b
tadosuke <tadosuke@gmail.com>
2022-08-17 23:03:47 +0900

    入力コールバックをinput側に作成

--- src/input.py
+++ src/input.py
@@ -109,3 +109,48 @@ class OperationParam:
     def is_repeat(self) -> bool:
         """リピートされたか."""
         return self.state == InputState.Repeat
+
+
+class InputEvent:
+    """入力イベント."""
+
+    #: 入力イベントに対するコールバック
+    Callback = tp.Callable[[OperationParam], bool]
+    #: 入力コールバック辞書
+    CallbackDict = dict[VirtualKey, Callback]
+
+    def __init__(self):
+        self._callback_dict: InputEvent.CallbackDict = {}
+
+    def process(self, param: OperationParam) -> bool:
+        """入力を処理する.
+
+        :param param: 入力情報
+        :return: 処理されたか
+        """
+        callback = self._callback_dict.get(param.code)
+        if callback is None:
+            return False
+        processed = callback(param)
+        return processed
+
+    def connect(self, code: VirtualKey, callback: Callback) -> None:
+        """コールバックを登録する.
+
+        :param code: 抽象キー
+        :param callback: 抽象キーが押された際に呼ばれるコールバック
+        """
+        if code in self._callback_dict.keys():
+            raise ValueError(f'code({code}) is already registered.')
+        self._callback_dict[code] = callback
+
+    def disconnect(self, code: VirtualKey) -> None:
+        """コールバックの登録を解除する.
+
+        :param code: 抽象キー
+        """
+        del self._callback_dict[code]
+
+    def disconnect_all(self) -> None:
+        """全てのキーのコールバック登録を解除する."""
+        self._callback_dict.clear()
--- tests/test_input.py
+++ tests/test_input.py
@@ -17,6 +17,23 @@ class TestInput(unittest.TestCase):
         self.assertFalse(param.is_release())
         self.assertFalse(param.is_repeat())
 
+    def test_input_event(self):
+        event = InputEvent()
+
+        param = OperationParam(code=VirtualKey.MouseLeft, state=InputState.Press)
+        self.assertFalse(event.process(param))
+        event.connect(VirtualKey.MouseLeft, self._on_input)
+        self.assertTrue(event.process(param))
+        event.disconnect(VirtualKey.MouseLeft)
+        self.assertFalse(event.process(param))
+        event.connect(VirtualKey.MouseLeft, self._on_input)
+        event.disconnect_all()
+        self.assertFalse(event.process(param))
+
+    @staticmethod
+    def _on_input(param: OperationParam) -> bool:
+        return True
+
 
 if __name__ == '__main__':
     unittest.main()
