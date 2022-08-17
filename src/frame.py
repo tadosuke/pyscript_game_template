708bc5234d384de438e3c1bf5d54c3a16ba8caef
tadosuke <tadosuke@gmail.com>
2022-08-17 22:17:50 +0900

    Frameクラスに入力イベントを登録できるようにした

--- src/frame.py
+++ src/frame.py
@@ -1,7 +1,14 @@
 """フレームモジュール."""
 from __future__ import annotations
 
-from values import Rect
+import typing as tp
+from values import Rect, Position, Size
+from input import OperationParam, VirtualKey
+
+#: 入力イベントに対するコールバック
+InputCallback = tp.Callable[[OperationParam], bool]
+#: 入力コールバック辞書
+InputCallbackDict = dict[VirtualKey, InputCallback]
 
 
 class Frame:
@@ -14,12 +21,41 @@ class Frame:
             parent.append(self)
 
         self._children: list[Frame] = []
+        self._input_callback: InputCallbackDict = dict()
 
     def append(self, child: Frame) -> None:
         """子フレームを追加する."""
         if child in self._children:
-            raise RuntimeError
+            raise RuntimeError(f'Frame is already exists.')
         self._children.append(child)
+
+    def connect_input(self, code: VirtualKey, callback: InputCallback):
+        """入力コールバックを登録する."""
+        if code in self._input_callback.keys():
+            raise ValueError(f'code({code}) is already registered.')
+        self._input_callback[code] = callback
+
+    def disconnect_input(self, code: VirtualKey):
+        """入力コールバックの登録を解除する."""
+        del self._input_callback[code]
+
+    def process_input(self, param: OperationParam) -> bool:
+        """入力を処理する.
+
+        :return: 処理されたか
+        """
+
+        # 子
+        for frame in reversed(self._children):
+            if frame.process_input(param):
+                return True
+
+        # 自分
+        callback = self._input_callback.get(param.code)
+        if callback is None:
+            return False
+        processed = callback(param)
+        return processed
 
     @property
     def rect(self) -> Rect:
@@ -27,7 +63,14 @@ class Frame:
         return self._rect
 
     @property
+    def position(self) -> Position:
+        return self._rect.position
+
+    @property
+    def size(self) -> Size:
+        return self._rect.size
+
+    @property
     def parent(self) -> Frame:
         """親フレーム."""
         return self._parent
-
--- tests/test_frame.py
+++ tests/test_frame.py
@@ -4,12 +4,41 @@ import unittest
 
 from frame import *
 from values import *
+from input import *
 
 
 class TestFrame(unittest.TestCase):
 
     def test_case(self):
-        frame = Frame(Rect(Position(10, 20), Size(30, 40)))
+        frame1 = Frame(Rect(Position(10, 20), Size(30, 40)))
+
+        self.assertIsNone(frame1._input_callback.get(VirtualKey.MouseLeft))
+        frame1.connect_input(VirtualKey.MouseLeft, self._on_input)
+        self.assertIsNotNone(frame1._input_callback.get(VirtualKey.MouseLeft))
+
+        param = OperationParam(
+            code=VirtualKey.MouseLeft,
+            state=InputState.Press)
+        self.assertTrue(frame1.process_input(param))
+
+        param = OperationParam(
+            code=VirtualKey.MouseRight,
+            state=InputState.Press)
+        self.assertFalse(frame1.process_input(param))
+
+        frame2 = Frame(Rect(Position(30, 40), Size(50, 60)), parent=frame1)
+        self.assertEqual(frame2.parent, frame1)
+        self.assertEqual(frame1._children[0], frame2)
+
+        frame2.connect_input(VirtualKey.MouseMiddle, self._on_input)
+        param = OperationParam(
+            code=VirtualKey.MouseMiddle,
+            state=InputState.Press)
+        self.assertTrue(frame1.process_input(param))  # 子フレームが受け取るか
+
+    @staticmethod
+    def _on_input(param: OperationParam) -> bool:
+        return True
 
 
 if __name__ == '__main__':
